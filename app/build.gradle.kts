52082aad1a870d5e1e3dada0cf36a89ab977a37c
Julius Dejon <juliusdejon@gmail.com>
2023-12-11 13:18:55 -0500

    Added firebase storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -54,5 +55,25 @@ dependencies {
 
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
+
+
+    // camera
+
+    // CameraX core library using the camera2 implementation
+    val camerax_version = "1.4.0-alpha02"
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    // If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    // If you want to additionally use the CameraX Extensions library
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
 
 }
--- app/src/main/java/com/example/lostandfound/ui/MainActivity.kt
+++ app/src/main/java/com/example/lostandfound/ui/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.lostandfound.ui
 
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import com.example.lostandfound.R
 import com.example.lostandfound.databinding.ActivityMainBinding
 import com.example.lostandfound.ui.guest.GuestActivity
 import com.example.lostandfound.ui.reporter.ReporterLoginActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.Locale
 
 class MainActivity : AppCompatActivity(), OnClickListener {
+    private val TAG: String = "MainActivity"
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,8 +41,10 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
         binding.btnReport.setOnClickListener(this)
 
-        //open intent Guest Activity
-//        binding.btnOpenMapView.setOnClickListener(this)
+        // open intent Guest Activity
+        // binding.btnOpenMapView.setOnClickListener(this)
+        binding.btnBrowser.setOnClickListener {
+        }
     }
 
     override fun onClick(v: View?) {
