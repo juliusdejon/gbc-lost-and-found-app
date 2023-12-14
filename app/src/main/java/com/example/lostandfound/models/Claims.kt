0adb9008d6059d27cde808cd5278d017ba44b77b
Sankarnath Suresh <sankarnathsuresh@gmail.com>
2023-12-13 19:38:39 -0500

    added claimsList screen

---
+++ app/src/main/java/com/example/lostandfound/models/Claims.kt
@@ -0,0 +1,4 @@
+package com.example.lostandfound.models
+
+class Claims {
+}
--- app/src/main/java/com/example/lostandfound/ui/owner/OwnerClaimsPage.kt
+++ app/src/main/java/com/example/lostandfound/ui/owner/OwnerClaimsPage.kt
@@ -1,48 +1,49 @@
 package com.example.lostandfound.ui.owner
 
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lostandfound.databinding.ActivityOwnerHomePageBinding
+import com.example.lostandfound.databinding.ActivityViewItemBinding
 import com.example.lostandfound.ui.guest.caseArrayList
 
-class OwnerHomePageActivity:AppCompatActivity() {
-    private lateinit var binding: ActivityOwnerHomePageBinding
+class OwnerClaimsPage:AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewItemBinding
     private var itemID : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityOwnerHomePageBinding.inflate(layoutInflater)
+
+        binding = ActivityViewItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         itemID = intent.getStringExtra("EXTRA_ID")
 
-        Log.d("sankar","here inside owner home page")
-
+        //Leo
         if (itemID != "") {
             for (i in caseArrayList) {
                 if (itemID == i.id) {
 
-                    Log.d("sankar","here inside owner home page for loop")
-
-
-                    this.binding.itemID.setText("You have claimed: ${i.id}")
+                    this.binding.itemID.setText("${i.id}")
                     this.binding.itemAddress.setText("${i.address}")
                     this.binding.itemType.setText("Type: ${i.type}")
 //                    this.binding.propertyCity.setText("${i.city}, ${i.postal}")
 //                    this.binding.propertySpecs.setText("Specifications: ${i.specs}")
-                    this.binding.itemDesciription.setText("You have claimed: ${i.description}")
-
-                    binding.btnViewClaims.setOnClickListener{
-                        val intent = Intent(this@OwnerHomePageActivity, OwnerHomePageActivity::class.java)
-                        intent.putExtra("EXTRA_ID", itemID)
-                        startActivity(intent)
+                    this.binding.itemDesciription.setText("${i.description}")
+                    if (i.isClaimed) {
+                        this.binding.itemIsClaimed.setText("CLAIMED")
+                        this.binding.itemIsClaimed.setTextColor(Color.rgb(1, 100, 32))
+                    } else {
+                        this.binding.itemIsClaimed.setText("UNCLAIMED")
+                        this.binding.itemIsClaimed.setTextColor(Color.rgb(255, 0, 0))
                     }
-                }
+                    this.binding.itemReporter.setText("Contact: ${i.reporter}")
+
+
                 }
             }
+                }
     }
 }
